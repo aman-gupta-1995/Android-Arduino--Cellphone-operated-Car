b70e67e146a1a314370f918b2f2fda3039c4021d
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-26 16:40:19 +0530

    Update ArduinoCarConsoleApp.java

--- ArduinoCarConsole/src/com/filderbaer/arduinocarconsole/ArduinoCarConsoleApp.java
+++ ArduinoCarConsole/src/com/filderbaer/arduinocarconsole/ArduinoCarConsoleApp.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;  
 import java.util.UUID;
 
-import android.app.Application;
+import android.app.Application; 
 import android.bluetooth.BluetoothDevice;   
 import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
