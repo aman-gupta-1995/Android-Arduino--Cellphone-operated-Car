a6fcd0092a85ccbc8ab3dc5339b293ccc266057a
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-26 16:39:44 +0530

    Update ArduinoCarConsoleApp.java

--- ArduinoCarConsole/src/com/filderbaer/arduinocarconsole/ArduinoCarConsoleApp.java
+++ ArduinoCarConsole/src/com/filderbaer/arduinocarconsole/ArduinoCarConsoleApp.java
@@ -2,7 +2,7 @@ package com.filderbaer.arduinocarconsole;
 
 import java.io.IOException;
 import java.io.InputStream; 
-import java.io.OutputStream;
+import java.io.OutputStream; 
 import java.util.UUID;
 
 import android.app.Application;
