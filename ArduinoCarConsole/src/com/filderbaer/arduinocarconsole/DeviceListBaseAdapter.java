9b2144ccccee47d6f91f5000e5c60aa07358ef8d
Gupta Aman <guptaaman@192.168.1.18>
2022-07-17 20:38:23 +0530

    changes

--- ArduinoCarConsole/src/com/filderbaer/arduinocarconsole/DeviceListBaseAdapter.java
+++ ArduinoCarConsole/src/com/filderbaer/arduinocarconsole/DeviceListBaseAdapter.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewGroup; 
 import android.widget.BaseAdapter;
-import android.widget.TextView; 
+import android.widget.TextView;  
 
 public class DeviceListBaseAdapter extends BaseAdapter {
 	
