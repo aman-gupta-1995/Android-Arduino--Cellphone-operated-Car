8a889fae1d0eaa2ff3d5ea694623dd433e59efe4
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-02 04:26:18 +0530

    Update BluetoothCar.ino

--- BluetoothCar/BluetoothCar.ino
+++ BluetoothCar/BluetoothCar.ino
@@ -2,7 +2,7 @@
 
 /**   
  * Motor shield (L298N)    
- */  
+ */   
 const int ENA = 5; // Port 5: Enable A        
 const int ENB = 6; // Port 6: Enable B          
 // IN1: 5v; IN2: GND -> Motor A Direction 1                   
